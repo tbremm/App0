e295a96f8635a4b87c080c08d2c347bf6ce556de
tbremm <juggling.engineer@gmail.com>
2014-09-13 01:34:58 -0400

    Broke the project, previous push left new graphics files behind.

--- gen/com/example/myfirstapp/R.java
+++ gen/com/example/myfirstapp/R.java
@@ -9,17 +9,6 @@ package com.example.myfirstapp;
 
 public final class R {
     public static final class attr {
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarButtonStyle=0x7f010001;
-        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-         */
-        public static final int buttonBarStyle=0x7f010000;
-    }
-    public static final class color {
-        public static final int black_overlay=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -28,14 +17,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
         /** 
 Refer to App Widget Documentation for margin information
 http://developer.android.com/guide/topics/appwidgets/index.html#CreatingLayout
     
          */
-        public static final int widget_margin=0x7f050002;
+        public static final int widget_margin=0x7f040002;
     }
     public static final class drawable {
         public static final int example_appwidget_preview=0x7f020000;
@@ -51,52 +40,49 @@ http://developer.android.com/guide/topics/appwidgets/index.html#CreatingLayout
         public static final int ic_stat_new_message=0x7f02000a;
     }
     public static final class id {
-        public static final int action_draw=0x7f09000a;
-        public static final int action_search=0x7f090009;
-        public static final int action_settings=0x7f090008;
-        public static final int activity_draw=0x7f090000;
-        public static final int activity_plot=0x7f090006;
-        public static final int button_newActivity=0x7f090003;
-        public static final int button_plot=0x7f090005;
-        public static final int button_toast=0x7f090004;
-        public static final int edit_message=0x7f090002;
-        public static final int my_draw=0x7f090001;
-        public static final int my_plot=0x7f090007;
+        public static final int action_draw=0x7f080008;
+        public static final int action_search=0x7f080007;
+        public static final int action_settings=0x7f080006;
+        public static final int activity_plot=0x7f080004;
+        public static final int button_newActivity=0x7f080001;
+        public static final int button_plot=0x7f080003;
+        public static final int button_toast=0x7f080002;
+        public static final int edit_message=0x7f080000;
+        public static final int my_plot=0x7f080005;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
-        public static final int activity_draw=0x7f030001;
-        public static final int activity_main=0x7f030002;
-        public static final int activity_plot=0x7f030003;
-        public static final int button1=0x7f030004;
-        public static final int fragment_main=0x7f030005;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_plot=0x7f030002;
+        public static final int button1=0x7f030003;
+        public static final int fragment_main=0x7f030004;
     }
     public static final class menu {
-        public static final int display_message=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int main_activity_actions=0x7f080002;
-        public static final int plot=0x7f080003;
+        public static final int display_message=0x7f070000;
+        public static final int main=0x7f070001;
+        public static final int main_activity_actions=0x7f070002;
+        public static final int plot=0x7f070003;
     }
     public static final class string {
-        public static final int action_draw=0x7f060008;
-        public static final int action_reply=0x7f06000d;
-        public static final int action_search=0x7f060007;
-        public static final int action_settings=0x7f060001;
-        public static final int action_share=0x7f06000c;
-        public static final int app_name=0x7f060000;
-        public static final int button_plot=0x7f06000e;
-        public static final int button_send=0x7f060003;
-        public static final int button_toast=0x7f060009;
-        public static final int dummy_button=0x7f060011;
-        public static final int dummy_content=0x7f060012;
-        public static final int edit_message=0x7f060002;
-        public static final int hello_world=0x7f060006;
-        public static final int new_message_notification_placeholder_text_template=0x7f06000b;
-        public static final int new_message_notification_title_template=0x7f06000a;
-        public static final int title_activity_display_message=0x7f060005;
-        public static final int title_activity_draw=0x7f060010;
-        public static final int title_activity_main=0x7f060004;
-        public static final int title_activity_plot=0x7f06000f;
+        public static final int action_draw=0x7f050008;
+        public static final int action_reply=0x7f05000d;
+        public static final int action_search=0x7f050007;
+        public static final int action_settings=0x7f050001;
+        public static final int action_share=0x7f05000c;
+        public static final int app_name=0x7f050000;
+        public static final int button_plot=0x7f05000e;
+        public static final int button_send=0x7f050003;
+        public static final int button_toast=0x7f050009;
+        public static final int dummy_button=0x7f050011;
+        public static final int dummy_content=0x7f050012;
+        public static final int edit_message=0x7f050002;
+        public static final int hello_world=0x7f050006;
+        public static final int new_message_notification_placeholder_text_template=0x7f05000b;
+        public static final int new_message_notification_title_template=0x7f05000a;
+        public static final int title_activity_display_message=0x7f050005;
+        public static final int title_activity_draw=0x7f050010;
+        public static final int title_activity_main=0x7f050004;
+        public static final int title_activity_plot=0x7f05000f;
     }
     public static final class style {
         /** 
@@ -123,65 +109,23 @@ http://developer.android.com/guide/topics/appwidgets/index.html#CreatingLayout
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
-        public static final int ButtonBar=0x7f070003;
-        public static final int ButtonBarButton=0x7f070004;
+        public static final int AppTheme=0x7f060001;
+        public static final int ButtonBar=0x7f060003;
+        public static final int ButtonBarButton=0x7f060004;
         /**     <item name="android:actionBarTabTextStyle">@style/MyActionBarTabText</item> 
     <item name="android:actionMenuTextColor">@color/actionbar_text</item> 
          */
-        public static final int CustomActionBarTheme=0x7f070006;
-        public static final int FullscreenActionBarStyle=0x7f070005;
-        public static final int FullscreenTheme=0x7f070002;
+        public static final int CustomActionBarTheme=0x7f060006;
+        public static final int FullscreenActionBarStyle=0x7f060005;
+        public static final int FullscreenTheme=0x7f060002;
         /**  ActionBar styles 
   <item name="android:titleTextStyle">@style/MyActionBarTitleText</item> 
    <item name=android:background">@drawable/actionbar_background</item> 
          */
-        public static final int MyActionBar=0x7f070007;
+        public static final int MyActionBar=0x7f060007;
     }
-    public static final class styleable {
-        /** 
-         Declare custom theme attributes that allow changing which styles are
-         used for button bars depending on the API level.
-         ?android:attr/buttonBarStyle is new as of API 11 so this is
-         necessary to support previous API levels.
-    
-           <p>Includes the following attributes:</p>
-           <table>
-           <colgroup align="left" />
-           <colgroup align="left" />
-           <tr><th>Attribute</th><th>Description</th></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.example.myfirstapp:buttonBarButtonStyle}</code></td><td></td></tr>
-           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.example.myfirstapp:buttonBarStyle}</code></td><td></td></tr>
-           </table>
-           @see #ButtonBarContainerTheme_buttonBarButtonStyle
-           @see #ButtonBarContainerTheme_buttonBarStyle
-         */
-        public static final int[] ButtonBarContainerTheme = {
-            0x7f010000, 0x7f010001
-        };
-        /**
-          <p>This symbol is the offset where the {@link com.example.myfirstapp.R.attr#buttonBarButtonStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.example.myfirstapp:buttonBarButtonStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
-        /**
-          <p>This symbol is the offset where the {@link com.example.myfirstapp.R.attr#buttonBarStyle}
-          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
-
-
-          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
-or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
-          @attr name com.example.myfirstapp:buttonBarStyle
-        */
-        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
-    };
 }
